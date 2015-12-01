14ec17fb9aa850ef0e563e03287c5415acf139d8
halflife1125 <sahith99@gmail.com>
2015-12-01 18:18:43 +0100

    Create line_follower.py

---
+++ line_follower.py
@@ -0,0 +1,24 @@
+# 'Line Follower' program
+# Robot
+
+import pi2go, time
+
+pi2go.init()
+
+# Defining speed
+speed = 60
+
+try:
+  while True:
+  # Defining sensors
+  left = pi2go.irLeftLine ()
+  right = pi2go.irRightLine()
+    if left == right: # if both sensors are the same (either on or off)
+      pi2go.forward(speed)
+    elif left == True: # if left sensor is on
+      pi2go.spinRight(speed)
+    elif right == True #if right sensor is on
+      pi2go.spinLeft(speed)
+      
+finally: # cleanup
+  pi2go.cleanup()
